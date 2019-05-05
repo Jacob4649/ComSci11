c84f90b0faac796ea7e9844ac58ed1b5c0bbd9fb
Jacob4649 <jacob.klimczak@gmail.com>
2019-05-05 13:15:19 -0400

    finished unit5.6 question 5

--- src/unit5/assignment2/KlimczakA2Q5.java
+++ src/unit5/assignment2/KlimczakA2Q5.java
@@ -21,7 +21,7 @@ public class KlimczakA2Q5 {
 
 	static int m_number = 0; //number to guess
 	static int m_guess = 0; //guess
-	static int m_invalid = 3;
+	static int m_invalid = 1;
 	static boolean m_end = false; //end the program
 	
 	static Scanner m_input = new Scanner(System.in);
@@ -56,45 +56,54 @@ public class KlimczakA2Q5 {
 				
 				try {
 					m_guess = ScannerUtils.readIntRange(m_input, 1, 50);
-				} catch (InputMismatchException e) {
-					if (m_invalid < 3)
-						m_invalid++;
-					else
+					
+					System.out.println();
+					
+					if (m_guess == m_number) { //correct guess
+						System.out.println("Correct!");
+					} else if (Math.abs(m_guess - m_number) <= 3) { //really close
+						System.out.println("Almost got it!");
+					} else if (Math.abs(m_guess - m_number) <= 10) { //pretty close
+						System.out.println("Too " + (m_guess > m_number ? "high " : "low ") + "but close!");
+					} else if (m_guess > m_number) { //greater than selected number
+						System.out.println("Too high.");
+					} else { //less than selected number
+						System.out.println("Too low.");
+					}
+				
+					System.out.println();
+					
+				} catch (InputMismatchException e) { //throws exception if input is not within the closed interval provided as arguments to the readIntRange method
+					m_invalid++;
+					System.out.println();
+					System.out.println("Invalid input, " + (3 - m_invalid) + " invalid entries left before the game automatically ends.");
+					System.out.println();
+					if (m_invalid >= 3) //if 3 invalid answers have been provided, exit the loop
 						break;
 				}
 				
-				System.out.println();
 				
-				if (m_guess == m_number) { //correct guess
-					System.out.println("Correct");
-				} else if (Math.abs(m_guess - m_number) <= 3) { //really close
-					System.out.println("Almost Got It!");
-				} else if (Math.abs(m_guess - m_number) <= 10) { //pretty close
-					System.out.println("Too " + (m_guess > m_number ? "high " : "low ") + "but close!");
-				} else if (m_guess > m_number) { //greater than selected number
-					System.out.println("Too High");
-				} else { //less than selected number
-					System.out.println("Too Low");
-				}
-				
-				System.out.println();
 				
 			} while (m_number != m_guess);
 			
-			if (m_invalid == 3) {
-				System.out.println("You Lose!");
+			if (m_invalid == 3) { //if they quit before guessing the number
+				System.out.println("You lose!");
 				System.out.println("Nice try, better luck next time.");
 				System.out.println("The number was " + m_number);
 				continuePrompt();
-			} else {
-				System.out.println("You Win!");
+			} else { //if they guessed the number
+				System.out.println("You win!");
 				System.out.println("Congratulations!");
 				continuePrompt();
 			}
 			
+			System.out.println("Would you like to play another game? (y/n)"); //prompt for new game
+			m_end = !ScannerUtils.yesQuery(m_input);
+			
+			System.out.println();
+			
 		}
 		
-		System.out.println();
 		System.out.println("Terminating Program");
 
 	}
